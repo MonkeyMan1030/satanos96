8a0814647af67ee7f6782c6dd4efa0c2199248f7
MonkeyMan1030 <132142256+MonkeyMan1030@users.noreply.github.com>
2026-02-16 14:01:59 -0500

    Delete SatanOS98 directory

--- SatanOS98/js/data/names.js
+++
@@ -1,5 +0,0 @@
-// js/data/names.js
-window.SATANOS_NAMES = {
-  first: ["Alex","Jordan","Taylor","Casey","Morgan","Riley","Jamie","Avery","Drew","Sam","Quinn","Parker","Skyler","Cameron","Reese","Rowan","Kendall","Hayden","Charlie","Devon"],
-  last:  ["Graves","Holloway","Blackwood","Roth","Marrow","Crowley","Voss","Ashford","Grimm","Gallow","Thorne","Briar","Cinder","Wolfe","Harker","Knox","Sable","Vale","Mordane","Wraith"]
-};
